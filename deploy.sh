8bc6b19f6765f3570471baa8b0449dddafb41bcf
amoskalev <fruitbang91@gmail.com>
2023-09-05 22:42:39 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:fruitbang/maxidom.git master
+git push -f git@github.com:fruitbang/maxidom.git master:gh-pages
 cd -
